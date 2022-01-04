341cf0163b3efbd5703ad9752db6332a42a208d1
GuliaevAlex <guliaevalex@gmail.com>
2022-01-04 19:43:49 +0200

    Change public fields resouce, login, password to private

--- src/models/Record.java
+++ src/models/Record.java
@@ -1,9 +1,9 @@
 package models;
 
 public class Record {
-    public  String resource;
-    public  String login;
-    public  String password;
+    private String resource;
+    private String login;
+    private String password;
 
     public Record(String resource, String login, String password) {
         this.resource = resource;
@@ -11,36 +11,36 @@ public class Record {
         this.password = password;
     }
 
-    public  String getResource() {
+    public String getResource() {
         return resource;
     }
 
-    public  void setResource(String resource) {
+    public void setResource(String resource) {
         this.resource = resource;
     }
 
-    public  String getLogin() {
+    public String getLogin() {
         return login;
     }
 
-    public  void setLogin(String login) {
+    public void setLogin(String login) {
         this.login = login;
     }
 
-    public  String getPassword() {
+    public String getPassword() {
         return password;
     }
 
-    public  void setPassword(String password) {
+    public void setPassword(String password) {
         this.password = password;
     }
 
     @Override
     public String toString() {
-        return '\n'+"{" +
+        return '\n' + "{" +
                 "Resource='" + resource + '\'' +
                 ", Login='" + login + '\'' +
                 ", Password='" + password + '\'' +
-                '}' +'\n';
+                '}' + '\n';
     }
 }
